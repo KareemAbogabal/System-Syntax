751bdb9b6cf32929acb471f867bc3a6d905f87c9
Kareem Abogabal <kareemabogabal41@gmail.com>
2026-06-03 20:41:54 +0300

    Add Cryptor

---
+++ app/Cryption/Cryptor.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Cryption;
+
+class Cryptor {
+  private static function matrix() {
+    return collect([
+      collect([" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+      collect(["A", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]),
+      collect(["B", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A"]),
+      collect(["C", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B"]),
+      collect(["D", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C"]),
+      collect(["E", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"]),
+      collect(["F", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E"]),
+      collect(["G", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F"]),
+      collect(["H", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G"]),
+      collect(["I", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H"]),
+      collect(["J", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I"]),
+      collect(["K", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]),
+      collect(["L", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]),
+      collect(["M", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]),
+      collect(["N", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]),
+      collect(["O", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]),
+      collect(["P", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]),
+      collect(["Q", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"]),
+      collect(["R", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"]),
+      collect(["S", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"]),
+      collect(["T", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"]),
+      collect(["U", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]),
+      collect(["V", "V", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]),
+      collect(["W", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"]),
+      collect(["X", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]),
+      collect(["Y", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]),
+      collect(["Z", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"]),
+      collect([" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]),
+      collect(["a", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]),
+      collect(["b", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a"]),
+      collect(["c", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b"]),
+      collect(["d", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"]),
+      collect(["e", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d"]),
+      collect(["f", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e"]),
+      collect(["g", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f"]),
+      collect(["h", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g"]),
+      collect(["i", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h"]),
+      collect(["j", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i"]),
+      collect(["k", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]),
+      collect(["l", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]),
+      collect(["m", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]),
+      collect(["n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]),
+      collect(["o", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"]),
+      collect(["p", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"]),
+      collect(["q", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]),
+      collect(["r", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"]),
+      collect(["s", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"]),
+      collect(["t", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"]),
+      collect(["u", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"]),
+      collect(["v", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"]),
+      collect(["w", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"]),
+      collect(["x", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w"]),
+      collect(["y", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x"]),
+      collect(["z", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"])
+    ]);
+  }
+  public static function encryption($word) {
+    $rowIndex = [];
+    $colIndex = [];
+    $rowString = [];
+    $colString = [];
+    $matrix = self::matrix();
+    $getIndexRowRand;
+    $getIndexColRand;
+    $result = "";
+    foreach (str_split($word) as $char) {
+      $rowIndex = [];
+      $colIndex = [];
+      $rowString = [];
+      $colString = [];
+      foreach ($matrix as $i => $row) {
+        $j = $row->search($char);
+        if ($j !== false) {
+          $rowIndex[] = $i;
+          $colIndex[] = $j;
+          $rowString[] = $matrix[$i][0];
+          $colString[] = $matrix[0][$j];
+        };
+      };
+      $getIndexRowRand = array_rand($rowIndex);
+      // $getIndexColRand = array_keys($colIndex, $getIndexRowRand)[0];
+      $getIndexColRand = $colIndex[$getIndexRowRand];
+      $getStringRow = $matrix[$getIndexRowRand][0];
+      $getStringCol = $matrix[0][$getIndexColRand];
+      // $reversedRow = (7 * ($shuffleNewIndexRow - 11 + 52)) % 52; 
+      // $reversedCol = (7 * ($shuffleNewIndexCol - 11 + 26)) % 26;
+      $result .= $getStringRow . $getStringCol;
+    };
+    // dd($rowIndex, $colIndex, $rowString, $colString, $getIndexRowRand, $getIndexColRand, $result);
+    // dd($result);
+    // $reversedRow = (7 * ($shuffleNewIndexRow - 11 + 52)) % 52; 
+    // $reversedCol = (7 * ($shuffleNewIndexCol - 11 + 26)) % 26;
+    return $result;
+  }
+  public static function decryption($text) {
+    $matrix = self::matrix();
+    $result = '';
+    $chars = str_split($text);
+    $pairs = array_chunk($chars, 2);
+    foreach ($pairs as $pair) {
+      if (count($pair) < 2) {
+        continue;
+      };
+      $rowIndex = null;
+      $colIndex = null;
+      foreach ($matrix as $i => $row) {
+        $r = $row->search($pair[0]);
+        if ($r !== false) {
+          $rowIndex = $r;
+          break;
+        };
+      };
+      foreach ($matrix[0] as $j => $value) {
+        if ($value === $pair[1]) {
+          $colIndex = $j;
+          break;
+        };
+      };
+      if ($rowIndex !== null && $colIndex !== null) {
+        $result .= $matrix[$rowIndex][$colIndex];
+      };
+    };
+    // dd($result);
+    return $result;
+  }
+}
--- app/Http/Controllers/Website/Web/WelcomeController.php
+++ app/Http/Controllers/Website/Web/WelcomeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Website\Web;
 
 use App\Http\Controllers\Controller;
+use App\Cryption\Cryptor;
 
-class WelcomeController extends Controller
-{
-    public function index()
-    {
-        return view('website.web.welcome');
-    }
+class WelcomeController extends Controller {
+  public function index() {
+    Cryptor::encryption("Hello");
+    Cryptor::decryption("QRbDvQjChH");
+    return view('website.web.welcome');
+  }
 }
